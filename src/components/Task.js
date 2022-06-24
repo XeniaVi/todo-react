8cd9adac70b28442ad0dbb55359b9666a99be0f6
Ksenia Vislova <vislova.fortech@gmail.com>
2022-06-24 15:11:44 +0300

    feat: add removing of task by click

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ function Task({ item, deleteTask }) {
   return (
     <div>
       <li className="list-item">
-        <input type="checkbox"></input>
+        <input type="checkbox" />
         <span>{item.value}</span>
         <button onClick={() => deleteTask(item.id)}>x</button>
       </li>
