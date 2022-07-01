ee01534592b020e1d1b5102857fb13997fc61353
Ksenia Vislova <vislova.fortech@gmail.com>
2022-07-01 13:02:54 +0300

    refactor: remove changes for error message inside InputTask

--- src/components/InputTask.js
+++ src/components/InputTask.js
@@ -16,30 +16,28 @@ function InputTask({
   handleChangeInputCheckbox,
 }) {
   return (
-    <div>
-      <InputWrapper>
-        {isShowCheckbox ? (
-          <CheckboxAbsolute>
-            <input
-              type="checkbox"
-              onClick={toggleAllStatus}
-              onChange={handleChangeInputCheckbox}
-              checked={completedAll}
-            />
-          </CheckboxAbsolute>
-        ) : (
-          ""
-        )}
-        <Input
-          type="text"
-          placeholder="Add task"
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-          value={value}
-        />
-        <Button onClick={addTask}>Add</Button>
-      </InputWrapper>
-    </div>
+    <InputWrapper>
+      {isShowCheckbox ? (
+        <CheckboxAbsolute>
+          <input
+            type="checkbox"
+            onClick={toggleAllStatus}
+            onChange={handleChangeInputCheckbox}
+            checked={completedAll}
+          />
+        </CheckboxAbsolute>
+      ) : (
+        ""
+      )}
+      <Input
+        type="text"
+        placeholder="Add task"
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        value={value}
+      />
+      <Button onClick={addTask}>Add</Button>
+    </InputWrapper>
   );
 }
 
