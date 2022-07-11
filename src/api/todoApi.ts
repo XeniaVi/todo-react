8f4009b73ab30ceae0e02278aab91561ee58b30c
Ksenia Vislova <vislova.fortech@gmail.com>
2022-07-11 10:48:00 +0300

    feat: rewrite todoApi to ts

--- src/api/todoApi.ts
+++ src/api/todoApi.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { ITodo, UpdatedTodo } from "types/index.js";
 import { config } from "../config/config.js";
 
-export const getTodos = async (limit, offset, completed) => {
+export const getTodos = async (limit: number, offset: number, completed: boolean) => {
   const extra = typeof completed === "boolean" ? `&completed=${completed}` : "";
   const response = await axios.get(
     `${config.API_URL}?offset=${offset}&limit=${limit}${extra}`
@@ -9,25 +10,25 @@ export const getTodos = async (limit, offset, completed) => {
   return response.data;
 };
 
-export const addTodo = async (item) => {
+export const addTodo = async (item: ITodo) => {
   const response = await axios.post(config.API_URL, item);
   return response.data;
 };
 
-export const deleteTodo = async (id) => {
-  return axios.delete(`${config.API_URL}/${id}`, id);
+export const deleteTodo = async (id: string) => {
+  return axios.delete(`${config.API_URL}/${id}`, {data: id}); // проверить работу с такой передачей данных
 };
 
-export const updateTodo = async (id, updatedTodo) => {
+export const updateTodo = async (id: string, updatedTodo: UpdatedTodo) => {
   const response = await axios.put(`${config.API_URL}/${id}`, updatedTodo);
   return response.data;
 };
 
-export const deleteCompleted = async (ids) => {
+export const deleteCompleted = async (ids: Array<string>) => {
   return axios.delete(config.API_URL, { data: { ids } });
 };
 
-export const updateCompleted = async (ids, completed) => {
+export const updateCompleted = async (ids: Array<string>, completed: boolean) => {
   const response = await axios.put(config.API_URL, { ids, completed });
   return response.data;
 };
