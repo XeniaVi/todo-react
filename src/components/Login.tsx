b5f72871e4ebf39574055a78f22720713a196992
Ksenia Vislova <vislova.fortech@gmail.com>
2022-07-15 12:24:36 +0300

    feat: add layout for login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,16 @@
+import { ButtonForm, Container, Form, InputForm, Title } from "styles/components";
+
 const Login: React.FC = () => {
-  return (
-    <><h2>Login</h2></>
-  );
+    return (
+        <Container>
+            <Title>Login</Title>
+                <Form>
+                    <InputForm type="text" placeholder="username"/>
+                    <InputForm  type="password" placeholder="password"/>
+                    <ButtonForm type="submit">Sign in</ButtonForm>
+                </Form>
+        </Container>
+    );
 };
 
 export default Login;
