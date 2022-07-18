cfca0c407b01cfc92e736ea08678c9f3d568b82e
Ksenia Vislova <vislova.fortech@gmail.com>
2022-07-18 10:06:09 +0300

    feat: add validation for login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,54 @@
-import { ButtonForm, Container, Form, InputForm, Message, Title } from "styles/components";
-import { useAppSelector, useAppDispatch } from '../hooks'
+import { useState } from "react";
+import { useAppSelector, useAppDispatch } from '../hooks';
+import { setRegistrationError } from "slices/authSlice";
+import { ButtonForm, Container, Form, InputForm, Message, Title, LabelMessage } from "styles/components";
+import { minLengthPassword, maxLengthPassword } from "../constants";
 
 const Login: React.FC = () => {
     const dispatch = useAppDispatch();
 
     const message: string = useAppSelector((state) => state.auth.message);
+    const registrationError: string = useAppSelector((state) => state.auth.registrationError);
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChangeUsername = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setUsername(value);
+    };
+
+    const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = e.target;
+        setPassword(value);
+    };
+
+    const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+        e.preventDefault()
+        if (!username) {
+            dispatch(setRegistrationError('Username must not be empty'));
+        } else if (
+            password.length < minLengthPassword ||
+            password.length > maxLengthPassword
+        ) {
+            dispatch(setRegistrationError(`Password's length must be more than ${minLengthPassword} and less than ${maxLengthPassword}`));
+        } else {
+            console.log('login')
+        }
+    }
 
     return (
         <Container>
             <Title>Login</Title>
             {message && <Message>{message}</Message>}
-            <Form>
-                <InputForm type="text" placeholder="username" />
-                <InputForm type="password" placeholder="password" />
+            <Form onSubmit={handleSubmit}>
+                <div>
+                    {registrationError && (<LabelMessage>Incorrect value!</LabelMessage>)}
+                    <InputForm type="text" name="username" placeholder="username" onChange={handleChangeUsername} value={username} $mode={registrationError ? "error" : ""} />
+                </div>
+                <div>
+                    {registrationError && (<LabelMessage>Incorrect value!</LabelMessage>)}
+                    <InputForm type="password" name="password" placeholder="password" onChange={handleChangePassword} value={password} $mode={registrationError ? "error" : ""} />
+                </div>
                 <ButtonForm type="submit">Sign in</ButtonForm>
             </Form>
         </Container>
